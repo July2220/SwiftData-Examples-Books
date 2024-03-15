931762e1f6a04154249ffbad1071aa6f5419eded
July Tao <tjjsilwolf@gmail>
2024-03-15 10:47:06 +0800

    Initial Commit

---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by july on 2024/3/15.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
